ddcc790eaa687ceb158c69b6e71edc82c2cbffcf
Nir-Elk <nirelkayam31@gmail.com>
2019-08-28 21:28:58 +0300

    change profile

--- app/src/main/java/com/instagramy/activities/LoginActivity.java
+++ app/src/main/java/com/instagramy/activities/LoginActivity.java
@@ -1,9 +1,5 @@
 package com.instagramy.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +8,10 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,7 +24,7 @@ import com.instagramy.utils.Navigator;
 public class LoginActivity extends AppCompatActivity {
 
     private Navigator navigator;
-    private EditText userEmail,userPass;
+    private EditText userEmail, userPass;
     private ProgressBar loadingProgress;
     private Button LogBtn;
     private TextView LogNewAccount;
@@ -51,7 +51,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivityForResult(intent,REQUEST_CODE);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
         LogBtn.setOnClickListener(new View.OnClickListener() {
@@ -63,12 +63,12 @@ public class LoginActivity extends AppCompatActivity {
                 final String email = userEmail.getText().toString();
                 final String pass = userPass.getText().toString();
 
-                if(email.isEmpty() || pass.isEmpty()){
+                if (email.isEmpty() || pass.isEmpty()) {
                     showMessage("Please Verify all fields");
                     LogBtn.setVisibility(View.VISIBLE);
                     loadingProgress.setVisibility(View.INVISIBLE);
-                }else{
-                    singIn(email,pass);
+                } else {
+                    singIn(email, pass);
                 }
 
             }
@@ -80,7 +80,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         FirebaseUser user = mAuth.getCurrentUser();
-        if(user != null){
+        if (user != null) {
             //user is already connected to redirect him to home page
             navigator.navigate(MainActivity.class);
 
@@ -88,15 +88,14 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void singIn(String email, String pass) {
-        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     LogBtn.setVisibility(View.VISIBLE);
                     loadingProgress.setVisibility(View.INVISIBLE);
                     navigator.navigate(MainActivity.class);
-                }
-                else{
+                } else {
                     LogBtn.setVisibility(View.VISIBLE);
                     loadingProgress.setVisibility(View.INVISIBLE);
                     showMessage(task.getException().getMessage());
@@ -109,15 +108,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
-            if(resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 String result = data.getStringExtra("Result");
-                if(result.equals("OK"))
+                if (result.equals("OK"))
                     this.onStart();
             }
         }
     }
 
     private void showMessage(String text) {
-        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/instagramy/fragments/SettingsFragment.java
+++ app/src/main/java/com/instagramy/fragments/SettingsFragment.java
@@ -2,22 +2,42 @@ package com.instagramy.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.ImageViewTarget;
+import com.bumptech.glide.request.target.Target;
+import com.github.chrisbanes.photoview.PhotoView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.instagramy.R;
 import com.instagramy.activities.LoginActivity;
 import com.instagramy.activities.MainActivity;
+import com.instagramy.models.Profile;
 
 
 /**
@@ -33,12 +53,23 @@ public class SettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FirebaseAuth mAuth;
 
+    private ProgressBar userProgressBar;
+    private TextView emailProfile;
+    private EditText nameProfile, passProfile, rePassProfile;
+    private ImageView userImage;
+    private Button updatebtn;
     private Button logoutbtn;
     private Button aboutBtn;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabaseRef;
+    private Profile profile;
+    private View view;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -66,6 +97,8 @@ public class SettingsFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -86,7 +119,80 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View fragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
+        final View fragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
+        view = fragmentView;
+        this.database = FirebaseDatabase.getInstance();
+        this.mDatabaseRef = database.getReference().child("Profiles").child(mAuth.getCurrentUser().getDisplayName());
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                profile = dataSnapshot.getValue(Profile.class);
+                updateView(fragmentView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+
+
+        emailProfile = fragmentView.findViewById(R.id.user_email_profile);
+        nameProfile = fragmentView.findViewById(R.id.user_name_profile);
+        passProfile = fragmentView.findViewById(R.id.user_pass_profile);
+        rePassProfile = fragmentView.findViewById(R.id.user_repass_profile);
+        updatebtn = fragmentView.findViewById(R.id.user_update_profile_btn);
+        userProgressBar = fragmentView.findViewById(R.id.user_progressBar_profile);
+        userImage = fragmentView.findViewById(R.id.user_img_profile);
+
+
+        return fragmentView;
+    }
+
+    public void updateView(View fragmentView){
+
+        emailProfile.setText(profile.getEmail());
+        nameProfile.setText(profile.getName());
+        userProgressBar.setVisibility(View.VISIBLE);
+        userImage.setVisibility(View.VISIBLE);
+
+        Glide.with(fragmentView).load(profile.getImageUri()).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                userProgressBar.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                userProgressBar.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(userImage);
+
+        updatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(passProfile.getText().toString().equals(rePassProfile.getText().toString()) && passProfile.getText().toString().length()>5)
+                {
+                    mAuth.getCurrentUser().updatePassword(passProfile.getText().toString());
+
+                    // TODO: add notification
+                }
+                if(nameProfile.getText().toString().length()>0 && !nameProfile.getText().toString().equals(profile.getName()))
+                {
+                    mDatabaseRef.child("name").setValue(nameProfile.getText().toString());
+                    profile.setName(nameProfile.getText().toString());
+
+                    // TODO: add notification
+                }
+                passProfile.setText("");
+                rePassProfile.setText("");
+            }
+        });
+
         logoutbtn = fragmentView.findViewById(R.id.logout_btn);
         logoutbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,11 +205,8 @@ public class SettingsFragment extends Fragment {
         });
 
         aboutBtn = fragmentView.findViewById(R.id.about_btn);
-
         aboutBtn.setOnClickListener(Navigation.createNavigateOnClickListener(SettingsFragmentDirections.actionSettingsFragmentToAboutActivity()));
 
-
-        return fragmentView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
