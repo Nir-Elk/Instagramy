3c11b942326f107f1f88f8c9cbd89fe184e0995f
avivsegal <avivsegal@gmail.com>
2019-08-31 21:05:14 +0300

    Merge branch 'master' of https://github.com/Nir-Elk/Instagramy
    # Conflicts:
    #	app/src/main/java/com/instagramy/activities/SplashActivity.java

--- app/src/main/java/com/instagramy/activities/MainActivity.java
+++ app/src/main/java/com/instagramy/activities/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -43,14 +44,13 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.instagramy.R;
-import com.instagramy.helpers.FragmentsReacter;
 import com.instagramy.models.LinkListViewModel;
 import com.instagramy.models.Post;
 import com.instagramy.models.Profile;
 import com.instagramy.utils.GPSLocation;
 
 
-public class MainActivity extends FragmentsReacter {
+public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     private static final String ARGS_SCROLL_Y = "mStateScrollY";
     private FirebaseUser currentUser;
@@ -349,10 +349,6 @@ public class MainActivity extends FragmentsReacter {
             popupPostImage.setImageURI(pickedImgUri);
         }
 
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
     }
 
     @Override
